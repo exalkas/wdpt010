56f484a54f4c86e5a647e427df4b8e28eb186eba
Alkis Kastrisios <31480156+exalkas@users.noreply.github.com>
2022-11-17 19:53:04 +0200

    comments part2 added

---
+++ W20-SOCIAL2/02-comments/server/models/User.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose')
+const {Schema} = mongoose
+
+// userScheme is an instance of class Schema
+const userSchema = new Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+
+    password: {
+        type: String,
+        required: true,
+    },
+    image: {
+        type: String
+    },
+    address: {
+        type: String
+    },
+    age: {
+        type: String
+    }
+}) 
+
+// in db the collection should be 'users'
+module.exports = mongoose.model('User', userSchema)
